4163159a02bbc63d6c7177eb783d1be6d5faa083
dhuw <hazlewood.dyl@gmail.com>
2022-09-24 11:09:23 -0700

    bug test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooksdb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:<password>@cluster0.j2pwhyp.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
